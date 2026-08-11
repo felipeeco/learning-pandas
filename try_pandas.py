058fc22a4fd338d6fda547ece170bc4b1be6ec2c
Luis Felipe <felipemoreno.co@gmail.com>
2026-08-10 19:58:25 -0500

    change names data frame

--- try_pandas.py
+++ try_pandas.py
@@ -5,7 +5,16 @@ def get_ages_table() -> dict:
     df = pd.read_csv("data/Cars.csv", sep=";")
     df = df.dropna().reset_index()
 
-    print(df.dtypes)
+    index_rename = {
+        "chevrolet chevelle malibu": "chevelle malibu"
+    }
+    column_rename = {
+        "Categoria": "Clase"
+    }
+
+    df = df.set_index("Modelo")
+    df = df.rename(index=index_rename, columns=column_rename)
+    df = df.reset_index()
 
     return {
         "columns": df.columns.tolist(),
